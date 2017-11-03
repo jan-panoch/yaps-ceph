6df5633f4440d20d7ef76485d20a9f3eb404d88f
Joerg Eichhorn <joerg.eichhorn@kbb.eu>
2017-11-03 16:29:07 +0100

    comment

--- yapscephprtg.py
+++ yapscephprtg.py
@@ -22,8 +22,14 @@ import pprint
 import subprocess
 import sys
 
-
-
+#
+# try -h for info
+# intended as an extended script sensor for paessler prtg
+# transforming data from ceph -s 
+# so install in /var/prtg/scriptsxml/ and check executable flag
+# 
+# assumes ceph access - check read permissions to ceph.conf
+#
 
 
 CEPH_BIN = '/usr/bin/ceph'
